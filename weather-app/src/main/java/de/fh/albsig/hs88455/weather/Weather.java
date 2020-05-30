aec3517c5a2ef2f4b43dfd9669df24152fa483a1
Schmiels <56640760+Schmiels@users.noreply.github.com>
2020-05-30 16:10:09 +0200

    Update Weather.java
    Added some more important attributes

--- weather-app/src/main/java/de/fh/albsig/hs88455/weather/Weather.java
+++ weather-app/src/main/java/de/fh/albsig/hs88455/weather/Weather.java
@@ -9,6 +9,19 @@ public class Weather {
 
 	private int cityId;
 	private String cityName;
+	private String countryCode;
+	private String weatherDesc;
+	private double temp;
+	private double tempMax;
+	private double tempMin;
+	private int humidity;
+	private int pressure;
+	private int sunrise;
+	private int sunset;
+	private double lon;
+	private double lat;
+	private int windDeg;
+	private double windSpeed;
 	
 	public Weather(String cityName) {
 		this.cityName = cityName;
@@ -28,5 +41,109 @@ public class Weather {
 
 	public void setCityName(String cityName) {
 		this.cityName = cityName;
+	}
+
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getWeatherDesc() {
+		return weatherDesc;
+	}
+
+	public void setWeatherDesc(String weatherDesc) {
+		this.weatherDesc = weatherDesc;
+	}
+
+	public double getTemp() {
+		return temp;
+	}
+
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+
+	public double getTempMax() {
+		return tempMax;
+	}
+
+	public void setTempMax(double tempMax) {
+		this.tempMax = tempMax;
+	}
+
+	public double getTempMin() {
+		return tempMin;
+	}
+
+	public void setTempMin(double tempMin) {
+		this.tempMin = tempMin;
+	}
+
+	public int getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
+
+	public int getPressure() {
+		return pressure;
+	}
+
+	public void setPressure(int pressure) {
+		this.pressure = pressure;
+	}
+
+	public int getSunrise() {
+		return sunrise;
+	}
+
+	public void setSunrise(int sunrise) {
+		this.sunrise = sunrise;
+	}
+
+	public int getSunset() {
+		return sunset;
+	}
+
+	public void setSunset(int sunset) {
+		this.sunset = sunset;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public int getWindDeg() {
+		return windDeg;
+	}
+
+	public void setWindDeg(int windDeg) {
+		this.windDeg = windDeg;
+	}
+
+	public double getWindSpeed() {
+		return windSpeed;
+	}
+
+	public void setWindSpeed(double windSpeed) {
+		this.windSpeed = windSpeed;
 	}
 }
