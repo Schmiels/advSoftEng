b05715622ee9f40ee04f108c017009859a9e2072
Schmiels <56640760+Schmiels@users.noreply.github.com>
2020-06-02 09:46:30 +0200

    Create WeatherTest.java
    generated tests

---
+++ weather-app/src/test/java/de/fh/albsig/hs88455/weather/WeatherTest.java
@@ -0,0 +1,148 @@
+/**
+ * 
+ */
+package de.fh.albsig.hs88455.weather;
+
+/**
+ * @author svenb
+ *
+ */
+public class WeatherTest {	
+	
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#Weather(java.lang.String)}.
+	 */
+	@Test
+	public void testWeather() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setCityId(int)}.
+	 */
+	@Test
+	public void testSetCityId() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setCityName(java.lang.String)}.
+	 */
+	@Test
+	public void testSetCityName() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setCountryCode(java.lang.String)}.
+	 */
+	@Test
+	public void testSetCountryCode() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setWeatherDesc(java.lang.String)}.
+	 */
+	@Test
+	public void testSetWeatherDesc() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setTemp(double)}.
+	 */
+	@Test
+	public void testSetTemp() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setTempMax(double)}.
+	 */
+	@Test
+	public void testSetTempMax() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setTempMin(double)}.
+	 */
+	@Test
+	public void testSetTempMin() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setHumidity(int)}.
+	 */
+	@Test
+	public void testSetHumidity() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setPressure(int)}.
+	 */
+	@Test
+	public void testSetPressure() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setSunrise(int)}.
+	 */
+	@Test
+	public void testSetSunrise() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setSunset(int)}.
+	 */
+	@Test
+	public void testSetSunset() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setLon(double)}.
+	 */
+	@Test
+	public void testSetLon() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setLat(double)}.
+	 */
+	@Test
+	public void testSetLat() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setWindDeg(int)}.
+	 */
+	@Test
+	public void testSetWindDeg() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#setWindSpeed(double)}.
+	 */
+	@Test
+	public void testSetWindSpeed() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.fh.albsig.hs88455.weather.Weather#toXML()}.
+	 */
+	@Test
+	public void testToXML() {
+		//fail("Not yet implemented");
+	}
+
+}
